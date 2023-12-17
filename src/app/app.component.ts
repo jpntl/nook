c7a4a082f68c3c1d257e2a2f7bfc494777adb000
Joe Pantaliono <joe209446@gmail.com>
2023-12-17 01:09:20 -0600

    files

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, BaseComponent],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'nook';
+}
---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface User {
+    id: number,
+    name: string,
+    email: string,
+    activeStatus: boolean,
+    admin: boolean
+  }
